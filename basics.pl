5e45a70cbd81485a45f995731bad95ee68af50e5
AngelineRV <49433719+AngelineRV@users.noreply.github.com>
2019-04-30 12:06:13 +0530

    Add files via upload

---
+++ basics.pl
@@ -0,0 +1,38 @@
+# print"helloworld\n";
+# $name = <>;
+# print "MY NAME IS $name \n";
+
+# @names=("lenin","sugan","angel");
+# print "$names[2]\n","$names[1]\n";
+# print "@names \n";
+# print "ENTER YOUR NAME:\n";
+# $name = <>;
+# chomp ($name);
+# print "entered name is $name \n";
+# push (@names,$name,"amera","harish");
+# print "@names \n";
+
+# @Phones= <>;
+# chomp(@Phones);
+# print "@Phones";
+
+%parent= ('Angel'=>'Richard', 'harish'=>'selva');
+print "$parent{'harish'}\n";
+if (exists ($parent{"rigel"}))
+{
+print $parent{"rigel"};
+}
+else
+{
+print "Not found\n";
+}
+
+@students=keys%parent;
+print "@students\n";
+
+@parent=values%parent;
+print "@parent\n";
+delete $parent{'Angel'};
+print "@parent";
+
+
